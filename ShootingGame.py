41f8025b0ad7a6f8b646c5863178b5acad1988c7
semiza888 <164627267+semiza888@users.noreply.github.com>
2024-03-29 14:23:37 +0300

    add new const

--- ShootingGame.py
+++ ShootingGame.py
@@ -1,8 +1,13 @@
 import pygame
 
 
-
 running = True
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Shooting Game')
+icon = pygame.image.load('icon.png')
+
 while running:
     pass
 
